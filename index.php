cf156e7650bdaba7de2742c8a27df93b45bc7dde
mo-martin <momartin1@hotmail.co.uk>
2016-01-14 20:31:53 +0000

    New home contact

--- index.php
+++ index.php
@@ -21,26 +21,28 @@ include("includes/header.php"); ?>
 		<div class="sellingpoint"><h4>Flexible Viewings</h4></div>
 		<div class="sellingpoint"><h4>Flexible Deposit<br>Options</h4></div>
 	</section>
+	<section class="homemap">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d75983.66238492323!2d-2.230114!3d53.477584!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1451310597797" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+	</section>
 	<section class="homecontact">
-		<section class="contentleft map">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d75983.66238492323!2d-2.230114!3d53.477584!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1451310597797" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-		</section>
-		<section class="contentright flexcenter">
-			<table>
-			<tr>
-			<td><a href="info@davisbrook.co.uk"><img src="images/email.png" alt="Email Davisbrook Ltd" /></a></td>
-			<td><p><strong>Email –</strong> <a href="info@davisbrook.co.uk">info@davisbrook.co.uk</a></p></td>
-			</tr>
-			<tr>
-			<td><a href="01614397510"><img src="images/phone64.png" alt="Phone Davisbrook Office" /></a></td>
-			<td><p><strong>Office –</strong> <a href="tel:01614397510">0161 439 7510</a></p></td>
-			</tr>
-			<tr>
-			<td><a href="07879495614"><img src="images/mobile64.png" alt="Phone Davisbrook Mobile" /></a></td>
-			<td><p><strong>Mobile –</strong> <a href="tel:07879495614">07879 495614</a></p></td>
-			</tr>
-			</table>
-		</section>
+		<a href="mailto:info@davisbrook.co.uk">
+		<div class="homecontactarea"><img src="images/emailcircle.png" alt="Email - Davisbrook Ltd" />
+		<h2>Email</h2>
+		<h5>info@davisbrook.co.uk</h5>
+		</div>
+		</a>
+		<a href="tel:01614397510">
+		<div class="homecontactarea"><img src="images/phonecircle.png" alt="Phone Office - Davisbrook Ltd" />
+		<h2>Phone</h2>
+		<h5>01614 397510</h5>
+		</div>
+		</a>
+		<a href="tel:07879495614">
+		<div class="homecontactarea"><img src="images/mobilecircle.png" alt="Phone Mobile - Davisbrook Ltd" />
+		<h2>Mobile</h2>
+		<h5>07879 495614</h5>
+		</div>
+		</a>
 	</section>
 </main>
 
