e44be5af42ef42fa6493bbc9b369213cfe2d2f0a
mo-martin <momartin1@hotmail.co.uk>
2016-01-16 17:12:03 +0000

    New Contact Icons

--- contact.php
+++ contact.php
@@ -7,26 +7,42 @@ include("includes/header.php"); ?>
 	<div class="pagetitle">
 		<h1>Contact</h1>
 	</div>
-	<section class="content contact">
-		<section class="contentleft">
-			<p><strong>Email –</strong> <a href="info@davisbrook.co.uk">info@davisbrook.co.uk</a></p>
-			<p><strong>Office –</strong> <a href="tel:01614397510">0161 439 7510</a></p>
-			<p><strong>Mobile –</strong> <a href="tel:07879495614">07879 495614</a></p>
-			<a href="info@davisbrook.co.uk"><img src="images/email.png" alt="Email Davisbrook Ltd" /></a>
-			<a href="01614397510"><img src="images/phone64.png" alt="Phone Davisbrook Office" /></a>
-			<a href="07879495614"><img src="images/mobile64.png" alt="Phone Davisbrook Mobile" /></a>
-		</section>
-		<section class="contentright">
-			<div class="contactform">
-				<form id="contactform" name="contact" method="post" action="process.php" onSubmit="alert('Message Successfully Sent!')">
-				    <input type="text" name="Name" id="Name" size="30" placeholder="Name" required=""> 
-				    <input type="text" name="Email" id="Email" size="30" placeholder="Email" required=""> 
-				    <textarea name="Message" id="Message" placeholder="Message" required></textarea>
-				    <input id="submit" type="submit" name="submit" value="Send" />  
-				</form>
-			</div>
-		</section>
+	<section class="contact">
+		<a href="mailto:info@davisbrook.co.uk">
+		<div class="contactarea"><img src="images/emailcircle.png" alt="Email - Davisbrook Ltd" />
+		<h2>Email</h2>
+		<h5>info@davisbrook.co.uk</h5>
+		</div>
+		</a>
+		<a href="tel:01614397510">
+		<div class="contactarea"><img src="images/phonecircle.png" alt="Phone Office - Davisbrook Ltd" />
+		<h2>Phone</h2>
+		<h5>01614 397510</h5>
+		</div>
+		</a>
+		<a href="tel:07879495614">
+		<div class="contactarea"><img src="images/mobilecircle.png" alt="Phone Mobile - Davisbrook Ltd" />
+		<h2>Mobile</h2>
+		<h5>07879 495614</h5>
+		</div>
+		</a>
+		<a href="https://maps.google.com/maps?ll=53.477584,-2.503399&z=11&t=m&hl=en-GB&gl=GB&mapclient=embed">
+		<div class="contactarea"><img src="images/mapcircle.png" alt="Phone Mobile - Davisbrook Ltd" />
+		<h2>Address</h2>
+		<h5>Manchester, United Kingdom</h5>
+		</div>
+		</a>
 	</section>
+	<section class="contactform">
+		<form id="contactform" name="contact" method="post" action="process.php" onSubmit="alert('Message Successfully Sent!')">
+		    <input type="text" name="Name" id="Name" size="30" placeholder="Name" required=""> 
+			<input type="text" name="Email" id="Email" size="30" placeholder="Email" required=""> 
+			<textarea name="Message" id="Message" placeholder="Message" required></textarea>
+			<input id="submit" type="submit" name="submit" value="Send" />  
+		</form>
+	</section>
+	
+	
 </main>
 
 
--- index.php
+++ index.php
@@ -24,23 +24,29 @@ include("includes/header.php"); ?>
 	<section class="homemap">
 			<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d75983.66238492323!2d-2.230114!3d53.477584!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1451310597797" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
 	</section>
-	<section class="homecontact">
+	<section class="contact">
 		<a href="mailto:info@davisbrook.co.uk">
-		<div class="homecontactarea"><img src="images/emailcircle.png" alt="Email - Davisbrook Ltd" />
+		<div class="contactarea"><img src="images/emailcircle.png" alt="Email - Davisbrook Ltd" />
 		<h2>Email</h2>
 		<h5>info@davisbrook.co.uk</h5>
 		</div>
 		</a>
 		<a href="tel:01614397510">
-		<div class="homecontactarea"><img src="images/phonecircle.png" alt="Phone Office - Davisbrook Ltd" />
+		<div class="contactarea"><img src="images/phonecircle.png" alt="Phone Office - Davisbrook Ltd" />
 		<h2>Phone</h2>
 		<h5>01614 397510</h5>
 		</div>
 		</a>
 		<a href="tel:07879495614">
-		<div class="homecontactarea"><img src="images/mobilecircle.png" alt="Phone Mobile - Davisbrook Ltd" />
+		<div class="contactarea"><img src="images/mobilecircle.png" alt="Phone Mobile - Davisbrook Ltd" />
 		<h2>Mobile</h2>
 		<h5>07879 495614</h5>
+		</div>
+		</a>
+		<a href="https://maps.google.com/maps?ll=53.477584,-2.503399&z=11&t=m&hl=en-GB&gl=GB&mapclient=embed">
+		<div class="contactarea"><img src="images/mapcircle.png" alt="Phone Mobile - Davisbrook Ltd" />
+		<h2>Address</h2>
+		<h5>Manchester, United Kingdom</h5>
 		</div>
 		</a>
 	</section>
