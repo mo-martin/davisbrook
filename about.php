7c8059628ede5c041643490e002273e9ca0395d2
mo-martin <momartin1@hotmail.co.uk>
2016-01-17 18:50:26 +0000

    Finished About Page

--- about.php
+++ about.php
@@ -8,14 +8,19 @@ include("includes/header.php"); ?>
 		<h1>About</h1>
 	</section>
 	<section class="content">
-		<section class="contentleft flexcenter">
-			<div class="abouttext">
-				<p>We own and manage all our properties and maintain them to the highest standards, our properties have a very modern contemporary finish throughout and range from 1-8 Bedrooms within the most popular locations of Fallowfield and Rusholme.</p>
-				<p>We have extensive in house experience in assisting and resolving problems that arise with student rental accommodation and we ensure a genuine 24hr emergency helpline that will be answered by us and not a call centre. We are committed to provide a personalised service and we are always just a phone call away.</p>
-			</div>
+		<section class="aboutheader">
+		
 		</section>
-		<section class="contentright map">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d75983.66238492323!2d-2.230114!3d53.477584!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1451310597797" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+		<section class="abouttext">
+			<h2>About Davisbrook Ltd</h2>
+			<p>We own and manage all our properties and maintain them to the highest standards, our properties have a very modern contemporary finish throughout and range from 1-8 Bedrooms within the most popular locations of Fallowfield and Rusholme.</p>
+			<p>We have extensive in house experience in assisting and resolving problems that arise with student rental accommodation and we ensure a genuine 24hr emergency helpline that will be answered by us and not a call centre. We are committed to provide a personalised service and we are always just a phone call away.</p>
+		</section>
+		<section class="abouttext">
+			<h2>Why Choose Us?</h2>
+			<p>Years of experience in supplying high quality student houses</p>
+			<p>All houses are renovated before tenancy start date</p>
+			<p></p>
 		</section>
 	</section>
 </main>
--- includes/header.php
+++ includes/header.php
@@ -23,29 +23,7 @@
 				<a href="#skipnav">Skip to Content</a>!
 			</p>
 </div>
-<!--
-<header class="mainheader" role="banner">
-	<div class="headerbg">
-		<div class="headerleft"><img src="images/logowhite.png" alt="Davisbrook Housing Logo"/></div>
-		<div class="headerright">
-		<a href="tel:01614397510"><div class="headerright1"><img src="images/phone64.png" alt="Phone Davisbrook Office" /><h3>01614 397510</h3></div></a>
-		<a href="tel:07879495614"><div class="headerright2"><img src="images/mobile64.png" alt="Phone Davisbrook Mobile" /><h3>07879 495614</h3></div></a>
-		</div>
-	</div>
-</header>
 
-<nav class="mainnav" role="navigation">
-	<div class="responsivenav" id="navigation">
-		<button class="navtoggle">toggle</button>
-			<ul class="mainlinks">
-				<li role="menuitem"><a href="index.php">Home</a></li>
-				<li role="menuitem"><a href="about.php">About Us</a></li>
-				<li role="menuitem"><a href="properties.php">Our Properties</a></li>
-				<li role="menuitem"><a href="contact.php">Contact Us</a></li>
-			</ul>
-	</div>
-</nav>
--->
 <nav class="mainnav" role="navigation">
 	<div class="navlogo">
 	<a href="index.php"><img src="images/logo.png" alt="Davisbrook Housing Logo"/></a>
